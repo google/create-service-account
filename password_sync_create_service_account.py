4f99e1725b729f02d03a30a5464a28c171c86e43
Liron Newman <lironn@google.com>
2025-09-08 22:05:14 +0100

    Fix API URL.

--- password_sync_create_service_account.py
+++ password_sync_create_service_account.py
@@ -290,7 +290,7 @@ async def verify_api_access():
         # Admin SDK does not have a corresponding service.
         api_name = "Admin SDK"
         raw_api_response = execute_api_request(
-            f"https://content-admin.googleapis.com/admin/directory/v1/users/{admin_user_email}?fields=isAdmin",
+            f"https://admin.googleapis.com/admin/directory/v1/users/{admin_user_email}?fields=isAdmin",
             token)
       if api == "calendar-json.googleapis.com":
         api_name = service_name = "Calendar"
